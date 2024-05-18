a63ba8bdb3d2e9047fa2f19bf64e2d11983df62b
vishnukumar-p <90267883+vishnukumar-p@users.noreply.github.com>
2024-05-18 14:05:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,232 @@
+let blocks = document.getElementsByClassName("drawing-area")[0];
+let addEdge = false;
+let cnt = 0;
+let dist;
+
+let alerted = localStorage.getItem("alerted") || "";
+if (alerted !== "yes") {
+  alert(
+    "Read instructions before proceeding by clicking i-icon in the top-right corner"
+  );
+  localStorage.setItem("alerted", "yes");
+}
+
+const addEdges = () => {
+  if (cnt < 2) {  
+    alert("Create atleast two nodes to add an edge");
+    return;
+  }
+
+  addEdge = true;
+  document.getElementById("add-edge-enable").disabled = true;
+  document.getElementsByClassName("run-btn")[0].disabled = false;
+  dist = new Array(cnt + 1)
+    .fill(Infinity)
+    .map(() => new Array(cnt + 1).fill(Infinity));
+};
+
+let arr = [];
+
+const appendBlock = (x, y) => {
+  document.querySelector(".reset-btn").disabled = false;
+  document.querySelector(".click-instruction").style.display = "none";
+
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.top = `${y}px`;
+  block.style.left = `${x}px`;
+  block.style.transform = `translate(-50%,-50%)`;
+  block.id = cnt;
+
+  block.innerText = cnt++;
+
+  block.addEventListener("click", (e) => {
+    e.stopPropagation() || (window.event.cancelBubble = "true");
+    if (!addEdge) return;
+
+    block.style.backgroundColor = "coral";
+    arr.push(block.id);
+
+    if (arr.length === 2) {
+      drawUsingId(arr);
+      arr = [];
+    }
+  });
+  blocks.appendChild(block);
+};
+
+blocks.addEventListener("click", (e) => {
+  if (addEdge) return;
+  if (cnt > 12) {
+    alert("cannot add more than 12 vertices");
+    return;
+  }
+  console.log(e.x, e.y);
+  appendBlock(e.x, e.y);
+});
+
+const drawLine = (x1, y1, x2, y2, ar) => {
+  if (dist[Number(ar[0])][Number(ar[1])] !== Infinity) {
+    document.getElementById(arr[0]).style.backgroundColor = "#333";
+    document.getElementById(arr[1]).style.backgroundColor = "#333";
+    return;
+  }
+
+  console.log(ar);
+  const len = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+  const slope = x2 - x1 ? (y2 - y1) / (x2 - x1) : y2 > y1 ? 90 : -90;
+
+  dist[Number(ar[0])][Number(ar[1])] = Math.round(len / 10);
+  dist[Number(ar[1])][Number(ar[0])] = Math.round(len / 10);
+
+  const line = document.createElement("div");
+  line.id =
+    Number(ar[0]) < Number(ar[1])
+      ? `line-${ar[0]}-${ar[1]}`
+      : `line-${ar[1]}-${ar[0]}`;
+  line.classList.add("line");
+  line.style.width = `${len}px`;
+  line.style.left = `${x1}px`;
+  line.style.top = `${y1}px`;
+
+  let p = document.createElement("p");
+  p.classList.add("edge-weight");
+  p.innerText = Math.round(len / 10);
+  p.contentEditable = "true";
+  p.inputMode = "numeric";
+  p.addEventListener("blur", (e) => {
+    if (isNaN(Number(e.target.innerText))) {
+      alert("Enter valid edge weight");
+      return;
+    }
+    n1 = Number(p.closest(".line").id.split("-")[1]);
+    n2 = Number(p.closest(".line").id.split("-")[2]);
+    dist[n1][n2] = Number(e.target.innerText);
+    dist[n2][n1] = Number(e.target.innerText);
+  });
+  line.style.transform = `rotate(${
+    x1 > x2 ? Math.PI + Math.atan(slope) : Math.atan(slope)
+  }rad)`;
+
+  p.style.transform = `rotate(${
+    x1 > x2 ? (Math.PI + Math.atan(slope)) * -1 : Math.atan(slope) * -1
+  }rad)`;
+
+  line.append(p);
+  blocks.appendChild(line);
+  document.getElementById(arr[0]).style.backgroundColor = "#333";
+  document.getElementById(arr[1]).style.backgroundColor = "#333";
+};
+
+const drawUsingId = (ar) => {
+  if (ar[0] === ar[1]) {
+    document.getElementById(arr[0]).style.backgroundColor = "#333";
+    arr = [];
+    return;
+  }
+  x1 = Number(document.getElementById(ar[0]).style.left.slice(0, -2));
+  y1 = Number(document.getElementById(ar[0]).style.top.slice(0, -2));
+  x2 = Number(document.getElementById(ar[1]).style.left.slice(0, -2));
+  y2 = Number(document.getElementById(ar[1]).style.top.slice(0, -2));
+  drawLine(x1, y1, x2, y2, ar);
+};
+
+const findShortestPath = (el) => {
+    let visited = [];
+    let source = Number(el.previousElementSibling.value);
+    if (source >= cnt || isNaN(source)) {
+      alert("Invalid source");
+      return;
+    }
+    document.getElementById(source).style.backgroundColor = "grey";
+  
+    let parent = [];
+    parent[source] = -1;
+  
+    let distance = Array(cnt).fill(Infinity);
+    distance[source] = 0;
+  
+    for (let i = 0; i < cnt - 1; i++) {
+      for (let u = 0; u < cnt; u++) {
+        for (let v = 0; v < cnt; v++) {
+          if (dist[u][v] !== undefined) {
+            if (distance[v] > distance[u] + dist[u][v]) {
+              distance[v] = distance[u] + dist[u][v];
+              parent[v] = u;
+            }
+          }
+        }
+      }
+    }
+    indicatePath(parent, source);
+  };
+  
+
+const indicatePath = async (parentArr, src) => {
+  document.getElementsByClassName("path")[0].innerHTML = "";
+  for (i = 0; i < cnt; i++) {
+    let p = document.createElement("p");
+    p.innerText = "Node " + i + " --> " + src;
+    await printPath(parentArr, i, p);
+  }
+};
+
+const printPath = async (parent, j, el_p) => {
+  if (parent[j] === -1) return;
+  await printPath(parent, parent[j], el_p);
+  el_p.innerText = el_p.innerText + " " + j;
+
+  document.getElementsByClassName("path")[0].style.padding = "1rem";
+  document.getElementsByClassName("path")[0].appendChild(el_p);
+
+  if (j < parent[j]) {
+    let tmp = document.getElementById(`line-${j}-${parent[j]}`);
+    await colorEdge(tmp);
+  } else {
+    let tmp = document.getElementById(`line-${parent[j]}-${j}`);
+    await colorEdge(tmp);
+  }
+};
+
+const colorEdge = async (el) => {
+  if (el.style.backgroundColor !== "aqua") {
+    await wait(1000);
+    el.style.backgroundColor = "aqua";
+    el.style.height = "8px";
+  }
+};
+
+const clearScreen = () => {
+  document.getElementsByClassName("path")[0].innerHTML = "";
+  let lines = document.getElementsByClassName("line");
+  for (line of lines) {
+    line.style.backgroundColor = "#EEE";
+    line.style.height = "5px";
+  }
+};
+
+const resetDrawingArea = () => {
+  blocks.innerHTML = "";
+
+  const p = document.createElement("p");
+  p.classList.add("click-instruction");
+  p.innerHTML = "Click to create node";
+
+  blocks.appendChild(p);
+  document.getElementById("add-edge-enable").disabled = false;
+  document.querySelector(".reset-btn").disabled = true;
+  document.getElementsByClassName("path")[0].innerHTML = "";
+
+  cnt = 0;
+  dist = [];
+  addEdge = false;
+};
+
+const wait = async (t) => {
+  let pr = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("done!");
+    }, t);
+  });
+  res = await pr;
+};
